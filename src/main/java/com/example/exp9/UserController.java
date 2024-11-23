46f0156047f160cedf883659099f5a394dfa8a65
mohammad <mohammad@gmail.com>
2024-11-23 17:47:55 +0200

    Initial commit

---
+++ src/main/java/com/example/exp9/UserController.java
@@ -0,0 +1,39 @@
+package com.example.exp9;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+@RestController
+public class UserController {
+    @Autowired
+    UserService userService;
+
+    @RequestMapping("/hello")
+    public String sayHello() {
+        return userService.sayHello();
+    }
+
+    @RequestMapping("/users") public ArrayList<User> getAllUsers() {
+        return userService.getUserList();
+    }
+
+    @RequestMapping(method= RequestMethod.POST, value="/users")
+    public boolean addUser(@RequestBody User user) {
+        return userService.addUserToUserList(user);
+    }
+
+    @RequestMapping(method=RequestMethod.DELETE,value="/users/{id}")
+    public boolean deleteUser(@PathVariable Integer id) {
+        // Delete with id from the User Service Array List
+        return userService.deleteUserToUserList(id);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value="/users/{id}")
+    public boolean updateUser(@RequestBody User user,@PathVariable Integer id) {
+        // Update with id in the User Service Array List }
+        return userService.updateUserToUserList(user, id);
+    }
+
+}
